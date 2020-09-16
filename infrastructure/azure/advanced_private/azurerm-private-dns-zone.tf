d738d6d2260c135c14708563a52044de7e98a01d
Ray Kao <raykao@gmail.com>
2020-09-16 14:48:56 -0400

    adding in acr private link

---
+++ infrastructure/azure/advanced_private/azurerm-private-dns-zone.tf
@@ -0,0 +1,12 @@
+resource "azurerm_private_dns_zone" "acr" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = azurerm_resource_group.aks.name
+}
+
+
+resource "azurerm_private_dns_zone_virtual_network_link" "acr" {
+  name                  = "acrPrivDnsLink"
+  resource_group_name   = azurerm_resource_group.aks.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr.name
+  virtual_network_id    = azurerm_virtual_network.aks.id
+}
