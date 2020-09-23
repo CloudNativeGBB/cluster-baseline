6370742b2d3683092c03478c1fccf15c94a6299d
Ray Kao <raykao@gmail.com>
2020-09-23 00:03:07 -0400

    jumpbox is now part of provisioning script

--- infrastructure/azure/advanced_private/provision.sh
+++ infrastructure/azure/advanced_private/provision.sh
@@ -3,6 +3,7 @@
 export WORKING_DIR="$(pwd)"
 export NETWORKING_DIR=$WORKING_DIR"/networking"
 export AKS_DIR=$WORKING_DIR"/aks"
+export JUMPBOX_DIR=$WORKING_DIR"/jumpbox"
 
 # Setup Networking
 echo "Deploy Networking"
@@ -15,7 +16,7 @@ terraform apply -auto-approve tfplan
 echo "Networking deployed"
 
 
-# Setup ACR
+# Setup AKS
 echo "Deploy AKS into networking"
 
 cd $AKS_DIR
@@ -24,3 +25,13 @@ terraform plan -out tfplan
 terraform apply -auto-approve tfplan 
 
 echo "AKS deployed"
+
+# Setup JUMPBOX
+echo "Deploy JUMPBOX into networking"
+
+cd $JUMPBOX_DIR
+terraform init
+terraform plan -out tfplan
+terraform apply -auto-approve tfplan 
+
+echo "JUMPBOX deployed"
