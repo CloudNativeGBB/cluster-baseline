9608e70db36a58ac7ddde5196b28106e9949f930
Ray Kao <ray.kao@microsoft.com>
2020-12-08 17:27:07 -0500

    consistent naming and defaulting to latest k8s if not specified

--- infrastructure/azure/advanced_public/00-variables.tf
+++ infrastructure/azure/advanced_public/00-variables.tf
@@ -14,6 +14,7 @@ variable "location" {
 }
 
 variable "kubernetes_version" {
+  default = ""
   type = string
 }
 
--- infrastructure/azure/advanced_public/azurerm-vnet.tf
+++ infrastructure/azure/advanced_public/azurerm-vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "aks" {
-  name                = "AksDemoVnet${local.suffix}"
+  name                = "${local.prefix}-aks-demo-vnet-${local.suffix}"
   location            = azurerm_resource_group.aks.location
   resource_group_name = azurerm_resource_group.aks.name
   address_space       = ["10.0.0.0/16"]
